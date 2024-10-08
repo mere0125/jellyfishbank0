caef10f96cd5ab661217d8e3daa7cf69dd2f39b5
mere0125 <157430435+mere0125@users.noreply.github.com>
2024-10-08 01:28:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const balance = 7; // Set the Jellyfish Coin balance to 7
+  
+    // Display the balance on page load
+    document.getElementById('balance').innerText = `${balance} JFC`;
+  });
+  
